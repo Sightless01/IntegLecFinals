125c2d287988fc7e2b8e1434b5ee03e2684d2802
holaaaaaa <32945429+holaaaaaa@users.noreply.github.com>
2018-05-11 22:25:37 +0800

    Updated Client

--- Java Client/Client.java
+++ Java Client/Client.java
@@ -1,7 +1,7 @@
-import hangman.*;           // The package containing stubs
-import org.omg.CosNaming.*;  // Client_hello will use the naming service
+import hangman.*;           
+import org.omg.CosNaming.*;  
 import org.omg.CosNaming.NamingContextPackage.*;
-import org.omg.CORBA.*;      // All CORBA applications need these classes
+import org.omg.CORBA.*;      
 import java.util.*;
 import java.awt.Dimension;
 import javax.swing.JOptionPane;
@@ -13,43 +13,24 @@ import javax.swing.JPasswordField;
 import java.awt.*;
 import java.applet.Applet;
 
-//
-// Usage: java -cp [path] Client_hello -ORBInitialPort [Port]
-// [path] is the path that contains the "HelloWorld" directory 
-//      Ex:  [path]\HelloWord
-// [Port] is the same port number of "orbd -ORBInitialPort [Port]"
-//
-public class Client
-{
-	
-  public static void main(String args[])
-  {
+
+public class Client {	
+  public static void main(String args[]) {
     String name = "";
     String word = "";
 	String name_service="hangman";
-//
-//      create the properties:org.omg.CORBA.ORBInitialHost and org.omg.CORBA.ORBInitialPort
-//	Properties properties = System.getProperties();
-//      properties.put("org.omg.CORBA.ORBInitialHost",args[0]);
-//      properties.put( "org.omg.CORBA.ORBInitialPort",args[1]);
-//
+
     try{
       
-      System.out.println("Client>Creating and initializing the ORB");
-      ORB orb = ORB.init(args, null); //or ORB orb = ORB.init(args, properties);  to start the orb with the properties
-//      
-      System.out.println("Client>getting the root naming context");
+      ORB orb = ORB.init(args, null); 
       org.omg.CORBA.Object objRef = orb.resolve_initial_references("NameService");
-//
-//    it uses NamingConetExt Interoperable Naming Service
+
       NamingContextExt ncRef = NamingContextExtHelper.narrow(objRef); 
-//      
-      System.out.println("Client>Resolving the object reference in naming ["+name_service+"]");
+      	     
       NameComponent nc = new NameComponent(name_service, "");
       NameComponent path[] = {nc};
       Hangman hangman = HangmanHelper.narrow(ncRef.resolve(path));
-//      
-      System.out.println("Client>calling the Hello service...");
+      
 	  do {
 		JPanel panel = new JPanel(new BorderLayout());
 		JLabel jUserName = new JLabel("User Name");
@@ -68,7 +49,6 @@ public class Client
                 JOptionPane.showMessageDialog(new JFrame(), message, "Error",
                 JOptionPane.ERROR_MESSAGE);
 			}
-			System.out.println(word);
 		} else if (result == JOptionPane.CANCEL_OPTION){
 			System.exit(0);
 		}
@@ -79,9 +59,7 @@ public class Client
       hangmanUI.setVisible(true);     
     }catch(Exception e)
     {
-	System.out.println("Exception:\n"+e);
     }
-//
   }
 }
 
